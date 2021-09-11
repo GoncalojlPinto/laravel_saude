007024bcf98483632c77dac5755a7356be5f7cf6
Goncalo Pinto <goncalojlpinto@gmail.com>
2021-09-11 17:05:26 +0100

    added Medic

---
+++ database/migrations/2021_09_11_155037_create_medicos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMedicosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->string('address', 255);
+            $table->string('phone', 14);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicos');
+    }
+}
